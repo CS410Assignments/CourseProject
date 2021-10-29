8c53a3eefdad09212fd37e62b5e70cc2621da511
Jonathan Birkey <jonathanbirkey@Jonathans-MacBook-Pro.local>
2021-10-29 16:45:51 -0500

    adding first iteration of the stock_scrapper script

--- stock_scrapper.py
+++ stock_scrapper.py
@@ -6,12 +6,34 @@
     Python Version: 3.10.0
     Notes: S&P 500 company list pulled from https://datahub.io/core/s-and-p-500-companies#data
 """
+import csv
 import datetime
+import numpy as np
 import pandas as pd
 import time
 
 
+def get_sp500_tickers(file: str) -> list[str]:
+    tickers = []
+    with open(file, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader, None)  # skip csv file headers
+        for i in reader:
+            tickers.append(i[0])
+    return tickers
+
+
+def calculate_daily_change_rate(df: pd.DataFrame) -> None:
+    df['Change'] = np.zeros((len(df)))
+    df['Change Rate'] = np.zeros((len(df)))
+    for i in df.index:
+        df['Change'].iloc[i] = (df['Close'].iloc[i] - df['Open'].iloc[i])
+        df['Change Rate'].iloc[i] = (df['Close'].iloc[i] - df['Open'].iloc[i]) / df['Open'].iloc[i] * 100
+
+
 def main():
+    file_path = 'data/sp_500_constituents_as_of_20211022.csv'
+    tickers = get_sp500_tickers(file_path)
     ticker = 'MMM'
     period1 = int(time.mktime(datetime.datetime(2020, 1, 1, 23, 59).timetuple()))
     period2 = int(time.mktime(datetime.datetime(2020, 12, 31, 23, 59).timetuple()))
@@ -19,8 +41,11 @@ def main():
 
     query_str = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&' \
                 f'period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+
     df = pd.read_csv(query_str)
-    print(df)
+    calculate_daily_change_rate(df)
+
+    # TODO: calculate change and change rate for every ticker in the tickers list and save it as a single dataset
 
 
 if __name__ == "__main__":
