5db3bee2b251afbbb781df25364456d86111c1a9
Jonathan Birkey <jonathanbirkey@Jonathans-MacBook-Pro.local>
2021-10-29 13:26:55 -0500

    adding csv and initial framework for stock_scrapper.py

---
+++ stock_scrapper.py
@@ -0,0 +1,16 @@
+"""
+    File name: stock_scrapper.py
+    Author: Jonathan Birkey
+    Email: jbirke2@illinois.edu
+    Date created: 29 Oct 2021
+    Python Version: 3.8
+"""
+
+
+def main():
+    """S&P 500 company list pulled from https://datahub.io/core/s-and-p-500-companies#data"""
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
