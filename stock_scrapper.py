39345c1f5c603b3fd17b55fd6ff066ed4225a551
Jonathan Birkey <jonathanbirkey@Jonathans-MacBook-Pro.local>
2021-10-29 15:27:26 -0500

    can pull a single stock

--- stock_scrapper.py
+++ stock_scrapper.py
@@ -3,13 +3,24 @@
     Author: Jonathan Birkey
     Email: jbirke2@illinois.edu
     Date created: 29 Oct 2021
-    Python Version: 3.8
+    Python Version: 3.10.0
+    Notes: S&P 500 company list pulled from https://datahub.io/core/s-and-p-500-companies#data
 """
+import datetime
+import pandas as pd
+import time
 
 
 def main():
-    """S&P 500 company list pulled from https://datahub.io/core/s-and-p-500-companies#data"""
-    print("Hello World")
+    ticker = 'MMM'
+    period1 = int(time.mktime(datetime.datetime(2020, 1, 1, 23, 59).timetuple()))
+    period2 = int(time.mktime(datetime.datetime(2020, 12, 31, 23, 59).timetuple()))
+    interval = '1d'  # daily = 1d, weekly = 1wk, monthly = 1m
+
+    query_str = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={period1}&' \
+                f'period2={period2}&interval={interval}&events=history&includeAdjustedClose=true'
+    df = pd.read_csv(query_str)
+    print(df)
 
 
 if __name__ == "__main__":
