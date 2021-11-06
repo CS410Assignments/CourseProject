31d2230f86c977f8675ffb5c098245d95226c686
Mathew McDade <mathewmcdade@gmail.com>
2021-11-06 18:57:45 -0500

    ready to scrape

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,87 +1,68 @@
-from collections import defaultdict
 import config
 import json
 import pandas as pd
-import numpy as np
 import praw
 from psaw import PushshiftAPI
 import datetime as dt
 from time import sleep
 
+SUBREDDIT = "wallstreetbets"
 
 def main():
     reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret,
                          user_agent=config.user_agent)
     print(reddit.read_only)
-    # wsb = reddit.subreddit("wallstreetbets")
-    # print(wsb.display_name)
-    # print(wsb.title)
-    # print(wsb.description)
     ps_reddit = PushshiftAPI(reddit)
 
-    start = int(dt.datetime(2020, 1, 1, 0, 0, 0).timestamp())
-    end = int(dt.datetime(2020, 12, 1, 0, 0, 0).timestamp())
-    submissions = list(
-        ps_reddit.search_submissions(subreddit="wallstreetbets", q="MMM 3M Corporation", after=start,
-                                     before=end, limit=5))
-    for i, submission in enumerate(submissions):
-        print("Submission: " + str(i))
-        print("id: " + submission.id)  # TODO: do we really need this?
-        print("selftext: " + submission.selftext)
-        print("score: " + str(submission.score))
-        print("upvote_ratio: " + str(submission.upvote_ratio))
-        print("num_comments: " + str(submission.num_comments))
-        for j, comment in enumerate(submission.comments.list()):
-            print("Comment: " + str(j))
-            print("body: " + comment.body)
-            print("score: " + str(comment.score))
+    documents_data = None
+    with open('data/team_yoda_data.json', 'r') as documents_file:
+        documents_data = json.load(documents_file)
 
-    # documents = dict()
-    #
-    # # Load stock {ticker, name} from csv:
-    # stocks = pd.read_csv("data/sp_500_constituents_as_of_20211022.csv")
-    # for _, stock in stocks.iterrows():
-    #     # print(stock['Symbol'], stock['Name'])
-    #     documents[stock['Symbol']] = dict()
-    #     start = dt.datetime(2020, 1, 1, 0, 0, 0)
-    #     one_day = dt.timedelta(days=1)
-    #     while start.year < 2021:
-    #         start_epoch = int(start.timestamp())
-    #         end = start + one_day
-    #         end_epoch = int(end.timestamp())
-    #
-    #         documents[stock['Symbol']][start.strftime("%d%b%Y")] = dict()
-    #         documents[stock['Symbol']][start.strftime("%d%b%Y")]['reddit_data'] = dict()
-    #         documents[stock['Symbol']][start.strftime("%d%b%Y")]['reddit_data']['submissions'] = list()
-    #         test_submission = {
-    #             'id': "",
-    #             'selftext': "",
-    #             'score': 0,
-    #             'upvote_ratio': 0,
-    #             'num_comments': 0,
-    #             'comments': list()
-    #         }
-    #         test_comment = {
-    #             'self_text': "",
-    #             'score': 0
-    #         }
-    #         test_submission['comments'].append(test_comment)
-    #         documents[stock['Symbol']][start.strftime("%d%b%Y")]['reddit_data']['submissions'].append(test_submission)
-    #         # print(start)
-    #         # print(end)
-    #         # submissions = list(ps_reddit.search_submissions(subreddit="wallstreetbets", after=start, before=end, limit=5))
-    #         # for submission in submissions:
-    #         #     print("Submission: " + submission.selftext)
-    #         #     print("Score: " + str(submission.score))
-    #         #     print("Date: " + str(submission.created_utc))
-    #         #     for comment in submission.comments.list():
-    #         #         print("Comment: " + comment.body)
-    #         start = end
-    #         # sleep to avoid abusing the API
-    #         # sleep(2)
-    # # print(documents)
-    # with open('documents.json', 'w+') as documents_file:
-    #     json.dump(documents, documents_file)
+    # Load stock {Symbol, Name} from csv:
+    stocks = pd.read_csv("data/fomo_etf_as_of_20211031.csv")
+
+    for _, stock in stocks.iterrows():
+        print(stock['Symbol'] + " " + stock['Name'])
+        start = dt.datetime(2020, 1, 2, 0, 0, 0)
+        print(start.strftime("%Y-%m-%d"))
+        one_day = dt.timedelta(days=360)
+        while start.year < 2021:
+            end = start + one_day
+            if start.strftime("%Y-%m-%d") not in documents_data[stock['Symbol']]:
+                start = end
+                continue
+            start_epoch = int(start.timestamp())
+            end_epoch = int(end.timestamp())
+
+            documents_data[stock['Symbol']][start.strftime("%Y-%m-%d")]['reddit_data']['submissions'] = list()
+
+            submissions = list(
+                ps_reddit.search_submissions(subreddit=SUBREDDIT, q=stock['Symbol'], after=start_epoch, before=end_epoch, limit=10))
+            for submission in submissions:
+                submission_item = {
+                    'author': submission.author.name if submission.author else "",
+                    'selftext': submission.selftext if submission.selftext else "",
+                    'score': submission.score if submission.score else 0,
+                    'upvote_ratio': submission.upvote_ratio if submission.upvote_ratio else 0.0,
+                    'num_comments': submission.num_comments if submission.num_comments else 0,
+                    'comments': list()
+                }
+                for comment in submission.comments.list():
+                    comment_item = {
+                        'author': comment.author.name if comment.author else "",
+                        'body': comment.body if comment.body else "",
+                        'score': comment.score if comment.score else 0
+                    }
+                    submission_item['comments'].append(comment_item)
+                documents_data[stock['Symbol']][start.strftime("%Y-%m-%d")]['reddit_data']['submissions'].append(submission_item)
+
+            start = end
+            # sleep to avoid abusing the API
+            sleep(3)
+
+    with open('data/team_yoda_data.json', 'w+') as documents_file:
+        json.dump(documents_data, documents_file)
+
 
 if __name__ == '__main__':
     main()
