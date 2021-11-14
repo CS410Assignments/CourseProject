8fb87295956336daf92fb72c106d7b3ab091f790
Ben-Sutter <42955074+Ben-Sutter@users.noreply.github.com>
2021-11-13 20:43:10 -0600

    basic button stuff

--- popup.js
+++ popup.js
@@ -1,18 +1,5 @@
-// When the button is clicked, inject setPageBackgroundColor into current page
-searchButton.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+document.getElementById("searchButton").addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: setPageBackgroundColor,
-//   });
-  alert("I am an alert box!");
+  document.getElementById("queryTitle").innerHTML = document.getElementById('searchInput').value;
 });
-
-// The body of this function will be executed as a content script inside the
-// current page
-// function setPageBackgroundColor() {
-//   chrome.storage.sync.get("color", ({ color }) => {
-//     document.body.style.backgroundColor = color;
-//   });
-// }
