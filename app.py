a9a0410c3b88fe2842cfe9531abc6edbd1dc0a12
Natalie So <nataliesotszyui@gmail.com>
2022-11-09 18:37:24 -0800

    add progress report

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ for filename in glob.glob(os.path.join(path, '*.txt')):
 from gensim import corpora
 dictionary = corpora.Dictionary(text_data)
 corpus = [dictionary.doc2bow(text) for text in text_data]
+
 import pickle
 pickle.dump(corpus, open('corpus.pkl', 'wb'))
 dictionary.save('dictionary.gensim')
