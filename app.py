07a6a3a4db33a2a0a5a0dd431869e8b1a640c9b9
Alex <61956478+alexlin1822@users.noreply.github.com>
2022-12-03 17:29:26 -0500

    V1 Updated

--- app.py
+++ app.py
@@ -8,23 +8,16 @@ import nltk
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
 import os, glob
-
 import pickle
-
 import pyLDAvis
 import pyLDAvis.gensim_models as gensimvis
 from streamlit import components
 
-
-# run this project use:
-# streamlit run app.py
-
-# python -m spacy download en
+# Load "en_core_web_sm"
 nlp = spacy.load("en_core_web_sm")
-
 parser = English()
 
-
+# Functions
 def tokenize(text):
     lda_tokens = []
     tokens = parser(text)
@@ -39,11 +32,6 @@ def tokenize(text):
             lda_tokens.append(token.lower_)
     return lda_tokens
 
-
-nltk.download('wordnet')
-nltk.download('omw-1.4')
-
-
 def get_lemma(word):
     lemma = wn.morphy(word)
     if lemma is None:
@@ -51,14 +39,8 @@ def get_lemma(word):
     else:
         return lemma
 
-
 def get_lemma2(word):
     return WordNetLemmatizer().lemmatize(word)
-
-
-nltk.download('stopwords')
-en_stop = set(nltk.corpus.stopwords.words('english'))
-
 
 def prepare_text_for_lda(text):
     tokens = tokenize(text)
@@ -67,7 +49,12 @@ def prepare_text_for_lda(text):
     tokens = [get_lemma(token) for token in tokens]
     return tokens
 
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
+en_stop = set(nltk.corpus.stopwords.words('english'))
 
+# HTML Start here
 HTML = """<div style="overflow-x: auto; padding: 1rem">{}</div>"""
 
 st.header('Topic Modeling for Course Transcripts', )
@@ -96,12 +83,10 @@ NUM_TOPICS = 5
 ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=NUM_TOPICS, id2word=dictionary, passes=15)
 ldamodel.save('model5.gensim')
 topics = ldamodel.print_topics(num_words=4)
+
 for topic in topics:
-    # print(topic)
     st.write(topic)
 
 vis = gensimvis.prepare(ldamodel, corpus, dictionary)
-
 html_string = pyLDAvis.prepared_data_to_html(vis)
-
 components.v1.html(html_string, width=1300, height=800)
