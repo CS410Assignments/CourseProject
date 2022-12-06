9f39c97c78b77945f43641af1063b9ffa80089c7
Alex <61956478+alexlin1822@users.noreply.github.com>
2022-12-06 17:52:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ nltk.download('stopwords')
 en_stop = set(nltk.corpus.stopwords.words('english'))
 
 # HTML Start here
-HTML = """<div style="overflow-x: auto; padding: 1rem">{}</div>"""
+# HTML = """<div style="overflow-x: auto; padding: 1rem">{}</div>"""
+HTML = """<div style="overflow-wrap: break-word; padding: 1rem">{}</div>"""
 
 st.header('Topic Modeling for Course Transcripts', )
 course_name = st.text_input('Input your course number here and press enter (from 1 to 11):')
