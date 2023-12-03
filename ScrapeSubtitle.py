7619d23c50843b8b822432a849e9f529b2bbcf51
Christian Opperman <christian.opperman@gmail.com>
2023-12-03 17:23:23 -0500

    Generalize scraping script

--- ScrapeSubtitle.py
+++ ScrapeSubtitle.py
@@ -1,132 +1,173 @@
-import time
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import re
+import time
 
-driver = webdriver.Firefox()
-coursera_url = "https://www.coursera.org"
-# ---------------------------------------------------------
-
-def get_soup(url):
-    driver.get(url)
-    time.sleep(20)
-
-    # Get the page source and parse the HTML content
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
-    return soup
-
-def login():
-    # Open the login page
-    login_url = coursera_url + "/?authMode=login"
-    driver.get(login_url)
-
-    # Ensure the page has loaded
-    input("Login and navigate to the course page, then press 'Enter' here")
-
-def get_week_urls():
-
-    # current_url = driver.current_url
-    # print("Current" + current_url) # To Be Done
-    current_url = "https://www.coursera.org/learn/text-mining/home/week/1"
-
-    if "https://www.coursera.org/learn/" in current_url:
-        week_urls = []
-
-        soup = get_soup(current_url)
-        # Count number of weeks this course has
-        # num_weeks = len(soup.find_all()) # To Be Done
+from bs4 import BeautifulSoup
+from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
-        for i in range(6):
-            week_url = current_url[:-1] + str(i + 1)
-            week_urls.append(week_url)
-        print(week_urls)
-        return week_urls
-    else:
-        input("Navigate to the right page, then press 'Enter'.")
-        get_week_urls()
+class CourseraScraper:
+    def __init__(self) -> None:
 
-def get_lecture_urls(week_url):
-    lecture_urls = []
-    soup = get_soup(week_url)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.coursera_url = "https://www.coursera.org"
+        self.course_transcript_for_json = {}
+        # Login to Coursera to allow scraper to parse pages
+        CourseraScraperLogin(self.driver).login()
 
-    elements = soup.find_all("div", attrs={"data-test":"WeekSingleItemDisplay-lecture"})
-    for element in elements:
-        a_tag = element.find('a')
-        if a_tag and 'href' in a_tag.attrs:
-            href_value = a_tag['href']
-            lecture_urls.append(coursera_url + href_value)
+    def run_scraper(self):
+        # Parse course to get list of urls for each week to scrape
+        course_transcripts = []
+
+        course_parser = CourseraCourseParser(self.driver)
+        course_name = course_parser.course_name
+
+        # Parse each week url to get list of lecture URLs to scrape
+        for week_url in course_parser.week_urls:
+            week_str = "Week" + week_url.rsplit("/", 2)[-1]
+            week_parser = CourseraWeekParser(self.driver, week_url)
+            lecture_urls = week_parser.lecture_urls
+
+            week_transcripts = []
+
+            for lecture_url in lecture_urls:
+                lecture_title = lecture_url.rsplit("/", 2)[-1]
+                lecture_subtitles = week_parser.get_lecture_subtitles(lecture_url)
+                week_transcripts.append({lecture_title: lecture_subtitles})
+
+            print('*'*50)
+            print(week_str)
+            print(week_transcripts)
+            print('*'*50)
+
+            course_transcripts.append({week_str: week_transcripts})
+
+        self.course_transcript_for_json[course_name] = course_transcripts
+
+
+class CourseraScraperLogin:
+    def __init__(self, driver: webdriver.Chrome) -> None:
+        self.driver = driver
+        self.url = "https://www.coursera.org"
+
+    def login(self) -> None:
+        login_url = self.url + "/?authMode=login"
+        self.driver.get(login_url)
+
+
+class CourseraCourseParser:
+    def __init__(self, driver: webdriver.Chrome) -> None:
+        self.driver = driver
+        self.prompt = "Navigate to the home page for the course you wish to scrape and press enter"
+        self.parse_course()
+
+    def parse_course(self):
+        # TODO: Automatically parse course name
+        self.course_name = "TODO"
+        self.get_week_urls()
+
+    def get_week_urls(self) -> None:
+        """Initialize the URLs for each week of the course"""
+        input(self.prompt)
+        self.landing_page = self.driver.current_url
+        # Coursera defaults to saving the user's last accessed week, so need to get the true landing
+        # page once it's been navigated to
+        self.landing_page = self.landing_page.split('week')[0]
+
+        week_url_list = []
+        if "https://www.coursera.org/learn/" in self.landing_page:
+            self.driver.get(self.landing_page)
+            week_list_xpath_pattern = "//*[@class='cds-108 css-1mxkpit cds-110']"
+            # Need to make sure the element loads on the page before it can be scraped
+            try:
+                myElem = WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.XPATH, week_list_xpath_pattern)))
+            except TimeoutException:
+                print("Loading took too much time!")
+            # Get all elements from the sidebare containing links to the course's week lectures
+            week_elements = self.driver.find_elements(
+                By.XPATH,
+                week_list_xpath_pattern)
+
+            for week_number in range(1, len(week_elements)+1):
+                week_url_list.append(self.landing_page + f"week/{week_number}")
         else:
-            print("href attribute not found")
-    print(lecture_urls)
-    return lecture_urls
+            self.get_week_urls()
 
-def get_lecture_subtitles(lecture_url):
-
-    soup = get_soup(lecture_url)
-    subtitles = []
-
-    # Find all div elements contain subtitles
-    pattern = re.compile(r'\bcss-1shylkf\b')
-    elements = soup.find_all('div', class_=pattern)
-    if len(elements) == 0:
-        print("No value retrieved")
-    else:
-        print("Retrieved")
-
-    for element in elements:
-        # Extract the timestamp
-        button = element.find('button', class_='timestamp')
-        timestamp = button.contents[-1].strip()
-
-        # Extract all phrase elements and concatenate the text of all subtitles
-        phrases = element.find_all('div', class_='phrases')
-        text_content = ' '.join(phrase.get_text().strip() for phrase in phrases)
-
-        # Append the subtitles to the list as a dictionary
-        subtitles.append({'time': timestamp, 'text': text_content, 'url': lecture_url})
-
-    # Print or process the subtitles
-    # print(subtitles)
-    return subtitles
-
-# ---------------------------------------------------------
-
-# Set up options
-options = Options()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')  # This may be necessary in a headless environment
-options.add_argument('--disk-cache-dir=/Users/jnfng_w/Documents/Cofig')
-prefs = {"profile.managed_default_content_settings.images": 2}
-options.add_experimental_option("prefs", prefs)
+        self.week_urls = week_url_list
 
 
-login()
+class CourseraWeekParser:
+    def __init__(self, driver: webdriver.Chrome, week_url: str) -> None:
+        self.driver = driver
+        self.week_url = week_url
+        self.get_lecture_urls()
 
-# Get course name # To Be Done
-course_name = "Text Mining and Analytics"
-week_urls = get_week_urls()
-subtitles_of_course = []
-for week_url in week_urls:
+    def get_lecture_urls(self):
+        lecture_urls = []
+        soup = self.get_page_soup(self.week_url)
+        elements = soup.find_all("div", attrs={"data-test": "WeekSingleItemDisplay-lecture"})
 
-    # Which week
-    week = "Week " + week_url.rsplit("/", 2)[-1]
-    lecture_urls = get_lecture_urls(week_url)
-    subtitles_of_week = []
-    for lecture_url in lecture_urls:
+        for element in elements:
+            a_tag = element.find('a')
+            if a_tag and 'href' in a_tag.attrs:
+                href_value = a_tag['href']
+                lecture_urls.append("https://www.coursera.org" + href_value)
+            else:
+                print("href attribute not found")
+        self.lecture_urls = lecture_urls
 
-        # Get course title
-        course_title = lecture_url.rsplit("/",2)[-1]
-        lecture_subtitles = get_lecture_subtitles(lecture_url)
-        subtitles_of_week.append({course_title: lecture_subtitles})
-    subtitles_of_course.append({week: subtitles_of_week})
-subtitle_package = {course_name: subtitles_of_course}
+    def get_lecture_subtitles(self, lecture_url):
+        soup = self.get_page_soup(lecture_url)
+        subtitles = []
 
-# Writing a JSON file
-with open('subtitles.json', 'w') as json_file:
-    json.dump(subtitle_package, json_file, indent=4)
+        # Find all div elements contain subtitles
+        pattern = re.compile(r'\bcss-1shylkf\b')
+        elements = soup.find_all('div', class_=pattern)
+        if len(elements) == 0:
+            print("No value retrieved")
+        else:
+            print("Retrieved")
+
+        for element in elements:
+            # Extract the timestamp
+            button = element.find('button', class_='timestamp')
+            timestamp = button.contents[-1].strip()
+
+            # Extract all phrase elements and concatenate the text of all subtitles
+            phrases = element.find_all('div', class_='phrases')
+            text_content = ' '.join(phrase.get_text().strip() for phrase in phrases)
+
+            # Append the subtitles to the list as a dictionary
+            subtitles.append({'time': timestamp, 'text': text_content, 'url': lecture_url})
+
+        # Process the subtitles
+        return subtitles
+
+    def get_page_soup(self, url: str) -> BeautifulSoup:
+        # Take driver to specified URL
+        self.driver.get(url)
+        # Insert a sleep timer to avoid being flagged as a bot
+        time.sleep(2)
+
+        # get the page source and parse the HTML content into a BeautifulSoup object
+        parge_source = self.driver.page_source
+        soup = BeautifulSoup(parge_source, 'html.parser')
+
+        return soup
 
 
+if __name__ == "__main__":
+    scraper = CourseraScraper()
+    scraper.run_scraper()
+    print(scraper.course_transcript_for_json)
+
+    # Writing a JSON file
+    with open('subtitles.json', 'w') as json_file:
+        json.dump(scraper.course_transcript_for_json, json_file, indent=4)
